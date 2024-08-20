3da5f9c46f0aecfbf372c1a147797f0249de9cf4
Rajdeep Maulik <rajdeepmaulik@gmail.com>
2024-08-20 22:44:47 +0530

    Initial Commit

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,16 @@
+export const EnvironmentalVariables = {
+    production:false,
+
+    //Base Urls to access the end points using the http client get method
+    weatherApiLocationBaseURL:'https://weather338.p.rapidapi.com/locations/search?',
+    weatherApiForecastBaseURL:'https://weather338.p.rapidapi.com/weather/forecast?',
+    
+    //Variables for API key name and value
+    xRapidApiKeyName:'X-RapidAPI-Key',
+    xRapidApikeyValue:'3633d63d35msh3174c5628d8f25ep1fe9d4jsnd5939bfe8daa',
+
+    //Variables for host name and value
+    xRapidApiHostName:'X-RapidAPI-Host',
+    xRapidApiHostValue:'weather338.p.rapidapi.com'
+    
+}
